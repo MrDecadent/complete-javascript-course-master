4e3c898d30ddf252acfc787cdd3562417ffb8755
MrDecadent <mrdecadent1314@gmail.com>
2022-09-19 23:01:47 +0800

    The switch Statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -258,3 +258,111 @@ if(favourite !== 23){
 }
 */
 
+/*
+const hasDriverLicense = true;
+const hasGoodVision = true;
+const isTired = true;
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+if(shouldDrive && !isTired){
+    console.log('MrD is able to drive!');
+}else{
+    console.log('someone else should drive...');
+}
+*/
+
+////////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. 
+The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. 
+   Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, 
+            a team only wins if it has a higher score than the other team, 
+            and the same time a score of at least 100 points. 
+    HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! 
+            So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. 
+            Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+/*
+const dolphinsAvgScore = (96+108+89)/3;
+const koalasAvgScore = (88+91+110)/3;
+
+if(dolphinsAvgScore>koalasAvgScore){
+    console.log('Winner is Dolphins! 🏆');
+}else if(dolphinsAvgScore<koalasAvgScore){
+    console.log('Winner is Koalas! 🏆');
+}else{
+    console.log('Two teams be a draw');
+}
+*/
+
+/*
+// BONUS
+const dolphinsAvgScore = (97+112+101)/3;
+const koalasAvgScore = (109+95+106)/3;
+if( dolphinsAvgScore>koalasAvgScore && dolphinsAvgScore>=100){
+    console.log('Winner is Dolphins! 🏆');
+}else if(dolphinsAvgScore<koalasAvgScore && koalasAvgScore>=100){
+    console.log('Winner is Koalas! 🏆');
+}else if(dolphinsAvgScore === koalasAvgScore && koalasAvgScore>=100 && dolphinsAvgScore>=100){
+    console.log('Two teams be a draw');
+}else{
+    console.log('No one wins the trophy ')
+}
+*/
+
+const day = 'tuesday';
+
+switch(day){
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if(day === 'monday'){
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+}else if(day === 'tuesday'){
+    console.log('Prepare theory videos');
+}else if(day === 'wednesday' || day === 'tuesday'){
+    console.log('Write code examples');
+}else if(day === 'friday'){
+    console.log('Record videos');
+}else if(day === 'saturday' || day === 'sunday'){
+    console.log('Write code examples');
+}else{
+    console.log('Not a valid day!');
+}
