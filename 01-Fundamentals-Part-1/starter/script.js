f8de78f8cf8c1717ba13d0c778447227299becad
MrDecadent <159357github>
2022-09-20 11:45:52 +0800

    git account test

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -405,3 +405,4 @@ GOOD LUCK 😀
 const bill = 430;
 bill >= 50 && bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${bill*0.15}, and the total value ${bill*1.15}`) 
 : console.log(`The bill was ${bill}, the tip was ${bill*0.2}, and the total value ${bill*1.2}`);
+//test
