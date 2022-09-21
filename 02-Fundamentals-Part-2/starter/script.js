fe18f9893eeb2c1fd80dd54dbfe46ea0ab0ef1ab
MrDecadent <mrdecadent1314@gmail.com>
2022-09-21 23:36:22 +0800

    Coding Challenge #2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -171,6 +171,7 @@ const years = [1990,1697,2077,2010,1999];
 console.log(calcAge(years[0]));
 */
 
+/*
 const friends = ['海豹蝈蝈','八猪','大象蝈蝈'];
 //数组尾部添加元素 返回新数字的长度
 friends.push('鱼蝈蝈');
@@ -190,3 +191,46 @@ console.log(friends.indexOf('八猪'));
 console.log(friends.includes('八猪'));
 friends.push(23);
 console.log(friends.includes('23'));//false 会严格执行 不会类型转换
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: 
+Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, 
+   calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). 
+   Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+const calcTip = billValue => {
+    if(billValue >= 50 && billValue <= 300){
+        return billValue * 1.15;
+    }else{
+        return billValue * 1.2;
+    }
+}
+
+const calcTip2 = billValue => billValue >= 50 && billValue <= 300 ? billValue * 1.15 : billValue * 1.2;
+
+console.log(calcTip2(100));
+
+const bill = [125,555,44];
+console.log(calcTip(bill[0]));
+console.log(calcTip(bill[1]));
+console.log(calcTip(bill[2]));
+
+const total = [calcTip(bill[0]),calcTip(bill[1]),calcTip(bill[2])];
+console.log(total);
+
