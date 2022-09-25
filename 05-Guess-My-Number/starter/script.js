fbf8cce83caeb5fbbd013d54f4e7ec986cfd0128
MrDecadent <mrdecadent1314@gmail.com>
2022-09-25 18:20:00 +0800

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,14 @@
 
 //拿到class为'message'的文本内容
 console.log(document.querySelector('.message').textContent);
+//改变文本内容
+document.querySelector('.message').textContent = '🎉Correct Number';
+
+document.querySelector('.number').textContent = '11';
+
+document.querySelector('.score').textContent = '114';
+
+document.querySelector('.highscore').textContent = '514';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
