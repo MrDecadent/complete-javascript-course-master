95292e24e48c7c4a1775860dbf8e1e38502f688b
MrDecadent <mrdecadent1314@gmail.com>
2022-09-27 23:54:11 +0800

    Working With Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,8 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 //有三个相同class的button 所以需要querySelectorAll
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
+
+const removeHidden = () => {
+  //把modal中的hidden元素删除
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const addHidden = () => {
+  //点击modal的关闭按钮时，为modal添加hidden属性
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener('click', removeHidden);
 }
+
+//这里调用函数不要加括号 加了等于js一开始就执行
+btnCloseModal.addEventListener('click', addHidden);
+
+overlay.addEventListener('click', addHidden);
