9aebe945b6b4eabccf4e2c0a170621db3508f594
MrDecadent <mrdecadent1314@gmail.com>
2022-10-01 14:30:01 +0800

    Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -31,3 +32,48 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+*/
+
+console.log(me); //undefiend
+// console.log(job); //报错
+// console.log(year); //报错
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+console.log(addDecl(2, 3)); //正常输出
+//因为箭头函数和表达式函数已经是值了 不能在此之前使用
+// console.log(addExpr(2, 3)); //报错
+// console.log(addArrow(2, 3)); //报错
+
+//函数
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+//Examlpe
+//这里因为var的原因 numProducts是undefined 转义成了false
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products delete');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+//var会创建全局变量
+//window.y和window.z都是undefined
+console.log(x === window.x); //true
+console.log(y === window.y); //false
+console.log(z === window.z); //false
