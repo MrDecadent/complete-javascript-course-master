70687c76d480bd6c056081b8ac84db03b2618842
MrDecadent <mrdecadent1314@gmail.com>
2022-10-03 23:26:36 +0800

    Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flingthNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flingthNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH114', 2, 500);
+createBooking('LH114', 5);
+//跳过参数 undefined
+createBooking('LH114', undefined, 500);
