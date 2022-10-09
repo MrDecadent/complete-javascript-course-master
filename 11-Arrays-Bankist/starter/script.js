f1abcb7318b390147680da38a5c334774a9f6317
MrDecadent <mrdecadent1314@gmail.com>
2022-10-09 22:46:44 +0800

    Creating DOM Elements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  //清空容器中的记录
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i, arr) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -155,6 +174,7 @@ movements.forEach(function (movement, index, array) {
 // ...
 */
 
+/*
 //Map
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -174,3 +194,4 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, set) {
   console.log(`${value}: ${value}`);
 });
+*/
