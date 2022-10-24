de8057e380b1251c6bc0f4a2ea75ae751c44e2a6
MrDecadent <mrdecadent1314@gmail.com>
2022-10-24 23:04:08 +0800

    Selecting, Creating, and Deleting Element

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -32,3 +32,37 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+console.log(document.getElementById('section--1'));
+const allButton = document.getElementsByTagName('button');
+console.log(allButton);
+
+console.log(document.getElementsByClassName('btn'));
+
+//创建和插入元素
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookie for improved functionality and analytics';
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+//一个元素只会出现在一个地方 如果多个地方都需要的话 使用克隆
+// header.append(message.cloneNode(true));
+
+header.before(message);
+// header.after(message);
+
+//删除元素
+document.querySelector('.btn--close-cookie').addEventListener('click', () =>
+  //message.remove()
+  message.parentElement.removeChild(message)
+);
