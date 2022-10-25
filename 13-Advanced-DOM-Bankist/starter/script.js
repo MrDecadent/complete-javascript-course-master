3c2afb4bf60d7d677c7876dcbbeb0d60a9021de7
MrDecadent <mrdecadent1314@gmail.com>
2022-10-25 23:52:50 +0800

    Implementing Smooth Scrolling part1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -33,6 +33,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+/*
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
@@ -110,3 +111,12 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 logo.clasName = 'jonas';
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+});
