b05da5e737f8514e2cf96eea393b62b5a490b0f1
MrDecadent <mrdecadent1314@gmail.com>
2022-10-30 21:25:44 +0800

    P181 DOM Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -229,3 +229,28 @@ document.querySelector('.nav').addEventListener(
   false
 );
 */
+
+const h1 = document.querySelector('h1');
+
+// 向下 子元素
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// 向上 父元素
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// 兄弟元素
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
