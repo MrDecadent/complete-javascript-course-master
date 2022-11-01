43b408e59211ed6a0b93aa5cb410c964655ada1d
MrDecadent <mrdecadent1314@gmail.com>
2022-11-01 17:05:14 +0800

    Prototypal Inheritance on Built-In Object

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -40,3 +40,25 @@ console.log(jonas.species, matilda.species);
 // 但这个属性是原型的 不是实例的
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+//object.prototype(原型链的最高级)
+console.log(jonas.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+//扩展原型的方法 所有子类都能使用 但不建议这样做
+// 如果js下个版本加了同名的新方法 你不得不更变
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
