9239226462ea95e842a21b304157b48f15ee6c0e
MrDecadent <mrdecadent1314@gmail.com>
2022-11-13 23:56:44 +0800

    Our First AJAX Call_ XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,40 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  // API的请求不一定是立刻的 所以需要添加监听事件
+  request.addEventListener('load', function () {
+    // 等价于 const data = JSON.parse(this.responseText)[0];
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${
+          +data.population / (1000000).toFixed(1)
+        }</p>
+        <p class="country__row"><span>🗣️</span>${Object.keys(
+          data.languages
+        )}</p>
+        <p class="country__row"><span>💰</span>${Object.keys(
+          data.currencies
+        )}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('japan');
+getCountryData('usa');
