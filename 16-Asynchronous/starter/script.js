1e58f9be82f6c11e2ffa943bf3fcd8ee0fbdc9db
MrDecadent <mrdecadent1314@gmail.com>
2022-11-14 12:19:45 +0800

    Welcome to Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,6 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -17,14 +18,42 @@ const getCountryData = function (country) {
     console.log(data);
 
     const html = `
-  <article class="country">
+    <article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${
+            +data.population / (1000000).toFixed(1)
+          }</p>
+          <p class="country__row"><span>🗣️</span>${
+            Object.entries(data.languages)[0][1]
+          }</p>
+          <p class="country__row"><span>💰</span>${
+            Object.entries(data.currencies)[0][1].name
+          }</p>
+      </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('japan');
+getCountryData('usa');
+*/
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
         <h3 class="country__name">${data.name.common}</h3>
         <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${
-          +data.population / (1000000).toFixed(1)
-        }</p>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
         <p class="country__row"><span>🗣️</span>${
           Object.entries(data.languages)[0][1]
         }</p>
@@ -34,10 +63,54 @@ const getCountryData = function (country) {
     </div>
   </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  // API的请求不一定是立刻的 所以需要添加监听事件
+  request.addEventListener('load', function () {
+    // 等价于 const data = JSON.parse(this.responseText)[0];
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+
+    // Get Neighbour Country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // 2次请求
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data] = JSON.parse(this.responseText);
+      console.log(data);
+      renderCountry(data, 'neighbour');
+    });
   });
 };
 
-getCountryData('japan');
-getCountryData('usa');
+getCountryAndNeighbour('uk');
+
+// 回调地狱
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+        setTimeout(() => {
+          console.log('5 second passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
