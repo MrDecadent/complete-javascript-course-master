9ec263e2655fe87db4d989ccb0722636f7419d0d
MrDecadent <mrdecadent1314@gmail.com>
2022-11-23 02:54:51 +0800

    Exporting and Importing in ES6 Modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,22 @@
+// 所有module默认使用严格模式
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+console.log('Importing module');
+
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+// 导入全部变量
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// 导入默认
+import add, { cart } from './shoppingCart.js';
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js'; // 可以但是不推荐
+add('pizza', 2);
+add('bread', 5);
+add('apple', 4);
+// 导入module的文件里的操作会影响被导入的module里面的值
+console.log(cart);
