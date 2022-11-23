9b09ca4a053f4b068479b88171470aa91fef45b8
MrDecadent <mrdecadent1314@gmail.com>
2022-11-23 23:59:29 +0800

    op-Level await (ES2022) part 1

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -20,3 +20,20 @@ add('bread', 5);
 add('apple', 4);
 // 导入module的文件里的操作会影响被导入的module里面的值
 console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
