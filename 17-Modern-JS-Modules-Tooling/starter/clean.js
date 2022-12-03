09bee2ff97497caf6341352e7900f9279ab5b083
MrDecadent <mrdecadent1314@gmail.com>
2022-12-03 15:25:06 +0800

    Let's Fix Some Bad Code_ Part 1

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,56 +9,79 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const spendingLimits = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const getLimist = user => spendingLimits?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
+  // if (!user) user = 'jonas';
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+  // 2.1 Origin Code
+  // let lim;
+  // if (spendingLimits[user]) {
+  //   lim = spendingLimits[user];
+  // } else {
+  //   lim = 0;
+  // }
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
-};
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
+  // 2.2 三目运算符
+  // let limit = spendingLimits[user] === null ? spendingLimits[user] : 0;
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+  // 2.3
+  // const limit = spendingLimits?.[user] ?? 0;
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
+  // 2.4 使用函数
+  const limit = getLimist(user);
+
+  if (value <= limit) {
+    // 3.1 Origin Code
+    // budget.push({ value: -value, description: description, user: user });
+    // 3.2 变量名与参数名一致情况下可以省略
+    budget.push({ value: -value, description, user });
   }
 };
-check();
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const checkExpense = function () {
+  for (const entry of budget) {
+    // let lim;
+    // if (spendingLimits[entry.user]) {
+    //   lim = spendingLimits[entry.user];
+    // } else {
+    //   lim = 0;
+    // }
+
+    const limit = getLimist(entry.user);
+
+    if (entry.value < -limit) {
+      entry.flag = 'limit';
+    }
+  }
+};
+checkExpense();
 
 console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
+const logBigExpenses = function (limit) {
+  let output = '';
+  for (const entry of budget) {
+    // 4.2
+    // if (entry.value <= -limit) {
+    //   // 4.1
+    //   // output += entry.description.slice(-2) + ' / '; // Emojis are 2 chars
+    //   output += `${entry.description.slice(-2)} / `;
+    // }
+
+    output += entry.value <= -limit ? `${entry.description.slice(-2)} / ` : '';
   }
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+logBigExpenses(50);
