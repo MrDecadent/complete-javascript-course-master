6369feefad9a5e43385600d100124755217a6344
MrDecadent <mrdecadent1314@gmail.com>
2022-12-09 16:28:10 +0800

    Helpers and Configuration Files

---
+++ 18-forkify/starter/src/js/helpers.js
@@ -0,0 +1,22 @@
+import { TIMEOUT_SEC } from './config';
+
+export const getJSON = async function (url) {
+  try {
+    // 优先执行最快完成的
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
